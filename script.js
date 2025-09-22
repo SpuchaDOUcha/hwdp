91cfe26737fcae5e3960b5fdacdab53da32dab88
SpuchDOUcha <rdlugooolski09@gmail.com>
2025-09-22 13:15:17 +0200

    petlw

---
+++ script.js
@@ -0,0 +1,12 @@
+content = document.getElementById('content');
+const samochody = [];
+let x;
+do{
+    x - Number.parseInt(window.prompt('Podaj liczbę samochodów'));
+    }while(Number.isNaN(x))
+        for(let i = 0; let i: Number
+            samochody [] = window.prompt('Podaj markę samochodu')
+                for(const samochod of samochody){
+                    content.innerHTML  += 'samochod ${samochod} ma ${samochody.length} liter<br>';
+                }
+        
