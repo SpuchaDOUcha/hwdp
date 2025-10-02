8415257564c89e68186666dc87771336f714e9e9
SpuchDOUcha <rdlugooolski09@gmail.com>
2025-10-02 11:23:16 +0200

    zmiana

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 content = document.getElementById('content');
-const samochody = [];
+<script>const samochody = [];
+
 let x;
 do{
-    x - Number.parseInt(window.prompt('Podaj liczbę samochodów'));
+    x = Number.parseInt(window.prompt('Podaj liczbę samochodów'));
     }while(Number.isNaN(x))
-        for(let i = 0; let i: Number
-            samochody [] = window.prompt('Podaj markę samochodu')
-                for(const samochod of samochody){
-                    content.innerHTML  += 'samochod ${samochod} ma ${samochody.length} liter<br>';
-                }
-        
+        for(let i = 0; i < x; i++){
+            samochody[i] = window.prompt('Podaj markę samochodu');
+        }
+        for(const samochod of samochody){
+            content.innerHTML  += `samochod ${samochod} ma ${samochody.length} liter<br>`;
+        }
+
+</script>
